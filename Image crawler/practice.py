96f89202dc865d3f663728f743a57339c85554d4
hyunxyz <isbll.xyz@gmail.com>
2020-09-15 16:15:19 +0900

    1

--- Image crawler/practice.py
+++ Image crawler/practice.py
@@ -5,4 +5,4 @@ browser = webdriver.Chrome('chromedriver.exe')
 
 search_term = '스마트팜'
 url = "https://www.google.co.in/search?q=" + search_term + "&tbm=isch"
-browser.get(url)        #url이라는 주소를 주소창에 입력한 결과를 보여줌
+browser.get(url)        #url로 이동
--- Image crawler/practice_절대경로.py
+++ Image crawler/practice_절대경로.py
@@ -1,9 +1,10 @@
+import time
 from selenium import webdriver
 
 #절대 경로 지정할 경우 (파일생성 > 드라이버파일 옮김 > 경로 복사 )
 driver = webdriver.Chrome(executable_path='C:/Users/admin/Downloads/chromedriver/chromedriver.exe')
-driver.get('https://www.naver.com') 
+driver.get('https://www.naver.com')  #이동
 
 time.sleep(2)
 
-driver.close()
+driver.close()                       #quit도 가능
