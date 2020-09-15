ffd55d2b6e93c7e6a9ca01a88040953e2b11b15d
hyunxyz <isbll.xyz@gmail.com>
2020-09-15 14:51:09 +0900

    1

---
+++ Image crawler/practice.py
@@ -0,0 +1,4 @@
+from selenium import webdriver
+
+# working directory안에 chromedriver.exe 파일을 directly 넣을 경우 (VSCode에서도 추가)
+browser = webdriver.Chrome('chromedriver.exe')
---
+++ Image crawler/practice_절대경로.py
@@ -0,0 +1,4 @@
+from selenium import webdriver
+
+#절대 경로 지정할 경우 (파일생성 > 드라이버파일 옮김 > 경로 복사 )
+driver = webdriver.Chrome('C:/Users/admin/Downloads/chromedriver.exe')
