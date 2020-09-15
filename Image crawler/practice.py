8c7a3b440e43fbdf7006a784a35ad07deea13409
hyunxyz <isbll.xyz@gmail.com>
2020-09-15 15:33:01 +0900

    1

--- Image crawler/practice.py
+++ Image crawler/practice.py
@@ -1,4 +1,8 @@
 from selenium import webdriver
 
-# working directory안에 chromedriver.exe 파일을 directly 넣을 경우 (VSCode에서도 추가)
+# working directory안에 chromedriver.exe 파일을 directly 넣을 경우 (VSCode explorer 창에 드랙드롭)
 browser = webdriver.Chrome('chromedriver.exe')
+
+search_term = '스마트팜'
+url = "https://www.google.co.in/search?q=" + search_term + "&tbm=isch"
+browser.get(url)        #url이라는 주소를 주소창에 입력한 결과를 보여줌
--- Image crawler/practice_절대경로.py
+++ Image crawler/practice_절대경로.py
@@ -1,4 +1,9 @@
 from selenium import webdriver
 
 #절대 경로 지정할 경우 (파일생성 > 드라이버파일 옮김 > 경로 복사 )
-driver = webdriver.Chrome('C:/Users/admin/Downloads/chromedriver.exe')
+driver = webdriver.Chrome(executable_path='C:/Users/admin/Downloads/chromedriver/chromedriver.exe')
+driver.get('https://www.naver.com') 
+
+time.sleep(2)
+
+driver.close()
